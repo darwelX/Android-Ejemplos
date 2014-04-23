5002f56d57cd3008d33a3ace7af5582aa799ccd7
Darwel Alfonso Quintero Duran <darwel34@gmail.com>
2014-04-23 17:14:45 -0430

    Esta activity se hizo para comprobar que los cambios surten efecto en toda la aplicacion

---
+++ Idiomas/src/com/idiomas/android/SegundoActivity.java
@@ -0,0 +1,15 @@
+package com.idiomas.android;
+
+import com.android.idiomas.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SegundoActivity extends Activity{
+	
+	public void onCreate(Bundle actividades)
+	{
+		super.onCreate(actividades);
+		setContentView(R.layout.segundo_activity);
+	}
+}
